a71d2b1736a9188fca0f985be433e0fd0624b5d8
ameliaptrch <ameliaptrch@gmail.com>
2022-09-14 18:57:24 +0700

    menambahkan Tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Amelia Putri Chaerani',
+        'npm' : '2106751985'
+    }
+    return render(request,"katalog.html",context)
