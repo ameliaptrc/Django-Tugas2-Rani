9b7deadd21f63f08d824f4d3bfc412e2b5d04bdc
ameliaptrch <ameliaptrch@gmail.com>
2022-09-22 00:25:15 +0700

    deploy

--- project_django/settings.py
+++ project_django/settings.py
@@ -30,6 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 # Application definition
 
 INSTALLED_APPS = [
